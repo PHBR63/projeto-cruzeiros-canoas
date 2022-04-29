8bcaf5a6a3fe3e16cf01b5ff511bcb0e01f36483
Matheus Soares <matheus.http@hotmail.com>
2022-04-29 19:52:11 -0300

    initial commit

---
+++ cruzeiro.cabines/src/main/java/cruzeiro/cabines/CabinesApplication.java
@@ -0,0 +1,13 @@
+package cruzeiro.cabines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinesApplication.class, args);
+	}
+
+}
---
+++ cruzeiro.cabines/src/test/java/cruzeiro/cabines/CabinesApplicationTests.java
@@ -0,0 +1,13 @@
+package cruzeiro.cabines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
