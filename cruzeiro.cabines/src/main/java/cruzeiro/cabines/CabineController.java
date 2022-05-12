3d2dd102ae0a0bfcde9025c748dd3f057614c229
Matheus Soares <matheus.http@hotmail.com>
2022-05-12 19:53:26 -0300

    cabine check

--- cruzeiro.cabines/src/main/java/cruzeiro/cabines/CabineController.java
+++ cruzeiro.cabines/src/main/java/cruzeiro/cabines/CabineController.java
@@ -2,9 +2,9 @@ package cruzeiro.cabines;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,24 +30,41 @@ public class CabineController {
 	CabineDAO dao;
 	
 	@GetMapping("/obter")
-	public ResponseEntity<Iterable<CabineBean>> obterCabines() {
+	public ResponseEntity<Iterable<CabineBean>> obterCabines(Integer id) {
 		return new ResponseEntity<Iterable<CabineBean>>(dao.findAll(), HttpStatus.OK);
 
 	}
 	
+	@GetMapping("/obter/{id}")
+	public ResponseEntity<CabineBean> obterCabineId(@PathVariable Integer id) {
+
+		Optional<CabineBean> ret = dao.findById(id);
+
+		if (ret.isPresent())
+			return new ResponseEntity<CabineBean>(ret.get(), HttpStatus.OK);
+		else 
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+	}
+	
 	@GetMapping("/atualizar/{id}/{maxPessoas}/{precoPessoa}")
-	public ResponseEntity<String> atualizarCabine(@PathVariable Integer idCabine, @PathVariable Integer maxPessoas, @PathVariable Integer precoPessoa) {
-		dao.atualizarCabine(idCabine, maxPessoas, precoPessoa);
+	public ResponseEntity<CabineBean> atualizarCabine(@PathVariable Integer id, @PathVariable Integer maxPessoas, @PathVariable Double precoPessoa) {
+		ResponseEntity<CabineBean> response = obterCabineId(id);
+
+		CabineBean cabine = response.getBody();
+		cabine.setMaxPessoas(maxPessoas);
+		cabine.setPrecoPessoa(precoPessoa);
+		
+		dao.save(cabine);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	@GetMapping("/excluir/{id}")
-	public String excluirCabine(@RequestBody Integer idCabine) {
-		//dao.deleteById(idCabine);
-		System.out.println("entrei nessa bagaça, id: {idCabine}");
-		return "entrei nessa bagaça, id: {idCabine}";
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<String> excluirCabineId(@PathVariable Integer id) {
+		dao.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/criar")
 	public ResponseEntity<String> criarCabine(@Valid @RequestBody CabineBean cabine) {
 		dao.save(cabine);
--- cruzeiro.cabines/src/main/java/cruzeiro/cabines/CabineDAO.java
+++ cruzeiro.cabines/src/main/java/cruzeiro/cabines/CabineDAO.java
@@ -1,12 +1,14 @@
 package cruzeiro.cabines;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface CabineDAO extends CrudRepository<CabineBean, Integer>{
 	
-	@Query("UPDATE Cabine C SET maxPessoas = ?2, precoPessoa = ?3 WHERE C.idCabine = ?1")
-	Iterable<CabineBean> atualizarCabine(int id, int maxPessoas, int precoPessoa);
+	@Modifying
+	@Query("update Cabine C set C.idCabine = ?1, C.maxPessoas = ?2, C.precoPessoa = ?3 where C.idCabine = ?1")
+	Iterable<CabineBean> atualizarCabine(Integer id, Integer maxPessoas, Double precoPessoa);
 
 }
